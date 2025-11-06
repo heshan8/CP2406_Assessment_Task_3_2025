3bb38a0785d1fe27b46e6a26b3ad69b077b606c0
heshan8 <heshan988@gmail.com>
2025-11-06 23:53:22 +1000

    Add code to output status messages for clarity

--- deleteImgs.ps1
+++ deleteImgs.ps1
@@ -1,12 +1,22 @@
+# Clean up old simulation files so the simulation runs form scrartch every time.
+
+#Status messeges for clarity
+Write-Host "`nCleaning up previous run...`n"
+
 # Remove the 'images' directory if it exists
 if (Test-Path "images") {
     Remove-Item -Recurse -Force "images"
+    Write-Host "Removed old 'images' folder."
 }
 
 # Create the 'images' directory
 New-Item -ItemType Directory -Path "images" | Out-Null
+Write-Host "Created new 'images' folder."
 
 # Remove the 'result.mp4' file if it exists
 if (Test-Path "result.mp4") {
     Remove-Item -Force "result.mp4"
+    Write-Host "Removed old 'results.mp4'."
 }
+
+Write-Host "`nCleanup complete. Running Simulation...`n"
